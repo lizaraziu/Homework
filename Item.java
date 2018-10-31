5cf2891ab833a85caf6df4b4911a383ab6d4c260
Elizar <sam@example.com>
2018-10-31 10:52:15 +0200

    Item.java

---
+++ Item.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Item {
+
+    String name;
+    double price;
+    void displayItems() {
+        System.out.print("Name: " + name);
+        System.out.print("Price: " + price);
+    }
+
+
+}
+
